cc0a6f9443e17bf012ff070b0ea0da63086f515a
Ritik Raj Gupta <150698330+ritikrajg@users.noreply.github.com>
2025-07-12 03:56:53 +0530

    Delete backend directory

--- backend/utils/featureFunctions.js
+++
@@ -1,9 +0,0 @@
-export const nextBillingDate = () => {
-  const oneMonthFromNow = new Date();
-  oneMonthFromNow.setMonth(oneMonthFromNow.getMonth() + 1);
-  return oneMonthFromNow;
-};
-export const renewPlan = (user) => {
-  const today = new Date();
-  return !user?.nextBillingDate || user?.nextBillingDate <= today;
-};
